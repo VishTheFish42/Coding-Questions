00911c512d8b1ab6a38ca791d0573dd87a589496
Vishwa Prakash <121592353+VishTheFish42@users.noreply.github.com>
2026-01-11 14:35:38 -0800

    Create adjacent_elements.py

---
+++ Arrays and Strings/adjacent_elements.py
@@ -0,0 +1,23 @@
+'''
+ADJACENT ELEMENTS
+Given a two-dimensional array and the position of an element (rows and columns start with 1), return the values of all ADJACENT elements (neighbors that are not diagonal).
+'''
+
+def give_adjacent_elements(arr, r, c):
+    num_rows = len(arr)
+    if num_rows == 0:
+        return []
+   
+    num_cols = len(arr[0])
+
+    neighbors = []
+
+    for row in [-1, 0, 1]:
+        for col in [-1, 0, 1]:
+            if (row != 0 and col != 0) or (row == 0 and col == 0):
+                continue
+
+            if 0 <= r + row - 1 < num_rows and 0 <= c + col - 1 < num_cols:
+                neighbors.append(arr[r + row - 1][c + col - 1])
+    
+    return neighbors
