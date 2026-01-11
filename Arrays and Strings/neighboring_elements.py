6b2b620293d8c0cc0a56c27aa26738be7f95527f
Vishwa Prakash <121592353+VishTheFish42@users.noreply.github.com>
2026-01-11 14:33:27 -0800

    Create neighboring_elements.py

---
+++ Arrays and Strings/neighboring_elements.py
@@ -0,0 +1,26 @@
+'''
+NEIGHBORING ELEMENTS
+Given a two-dimensional array and the position of an element (rows and columns start with 1), return the values of all neighboring elements.
+'''
+
+def give_surrounding_elements(arr, r, c):
+    num_rows = len(arr)
+    if num_rows == 0:
+        return []
+   
+    num_cols = len(arr[0])
+
+    if r <= 0 or c <= 0 or r > len(arr) or c > len(arr[0]):
+        return "Index out of bounds"
+
+    neighbors = []
+
+    for row in [-1, 0, 1]:
+        for col in [-1, 0, 1]:
+            if row == 0 and col == 0:
+                continue
+
+            if 0 <= r + row - 1 < num_rows and 0 <= c + col - 1 < num_cols:
+                neighbors.append(arr[r + row - 1][c + col - 1])
+    
+    return neighbors
