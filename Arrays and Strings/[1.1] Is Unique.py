268a385d108bbe5cccc4f4b1d7c3ef782b248380
Vishwa Prakash <121592353+VishTheFish42@users.noreply.github.com>
2026-04-12 21:44:36 -0700

    Implement 'Is Unique' string algorithm
    Add initial implementation for the 'Is Unique' problem.

---
+++ Arrays and Strings/[1.1] Is Unique.py
@@ -0,0 +1,18 @@
+'''
+CHAPTER 1
+Arrays and Strings
+
+
+PROBLEM 1.1
+Is Unique
+
+
+DESCRIPTION
+Implement an algorithm to determine if a string has all unique characters.
+'''
+
+
+'''
+EXTENSION
+What if you cannot use additional data structures?
+'''
