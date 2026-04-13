966fbc7a50eed5ecab541539662c7f5f8f9befcc
Vishwa Prakash <121592353+VishTheFish42@users.noreply.github.com>
2026-04-12 23:51:54 -0700

    Add function to check for unique characters in string
    Implement is_unique function to check for unique characters in a string.

--- Arrays and Strings/[1.1] Is Unique.py
+++ Arrays and Strings/[1.1] Is Unique.py
@@ -8,7 +8,16 @@ Is Unique
 DESCRIPTION
 Implement an algorithm to determine if a string has all unique characters.
 '''
+def is_unique(s):
+    s_letter_hash = {}
 
+    for letter in s:
+        if letter in s_letter_hash:
+            return False
+        else:
+            s_letter_hash[letter] = 1
+    
+    return True
 
 '''
 EXTENSION
